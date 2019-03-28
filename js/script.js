88582768bcf04b537e14422c82e10eaa0a6ea828
BetskoDenis <47107535+BetskoDenis@users.noreply.github.com>
2019-03-28 20:42:28 +0200

    v2

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var h,
+    w;
+function magic() {
+   h = document.getElementById('height').value;
+    document.getElementById('width').value = +w;
+    alert(document.getElementById('height').value);
+    document.getElementById('one').style.height = 'h px';
+    // h = +h;
+    //
+    // h = ;
+    // w = document.getElementById('one').style.width;
+
+}
